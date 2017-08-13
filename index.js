9fc0aea6b6b440a6ab3e572d41d094f5e8054479
Jim Cramer <remarcmij@gmail.com>
2017-08-14 00:42:30 +0200

    final version from lecture

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
-function Request () {}
-function RenderList () {}
-function GetHtmlRepoList () {}
-function GetHtmlMemberList () {}
+// function getJSON(url, method) {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.addEventListener("load", () => resolve(xhr.responseText));
+//     xhr.addEventListener("error", () => reject(xhr.statusText));
+//     xhr.open(method, url);
+//     xhr.send();
+//   }).then(res => JSON.parse(res))
+// }
+
+const fetchJSON = url => fetch(url).then(res => res.json())
+
+const getHtmlRepoList = repos =>
+  repos.reduce((html, { url, name }) =>
+    html + `<li><a href="${url}">${name}</a></li>`
+    , '')
+
+const getHtmlMemberList = members =>
+  members.reduce((html, { login, avatar_url }) =>
+    html + `<li><h2>${login}</h2><img src="${avatar_url}" width=230px /></li>`
+    , '')
+
+const renderList = (selector, html) => {
+  const $parent = document.querySelector(selector)
+  $parent.innerHTML = html
+}
+
+const renderList2 = selector => html => {
+  const $parent = document.querySelector(selector)
+  $parent.innerHTML = html
+}
 
 const GitHubORG = 'HackYourFuture'
 const HYFReposApiEndpoint = `https://api.github.com/orgs/HackYourFuture/repos`
-const HYFMembersApiEndpiont = `https://api.github.com/orgs/${GitHubORG}/members`
- 
+const HYFMembersApiEndpoint = `https://api.github.com/orgs/${GitHubORG}/members`
+
 let $repoList = '.repo-list ul'
 let $memberList = '.member-list ul'
 
-Request(HYFReposApiEndpoint, 'GET')
-  .then(GetHtmlRepoList)
-  .then(RenderList.bind(null, $repoList))
-  .catch(RenderList.bind(null, $repoList, '<li>Error</li>'))
+fetchJSON(HYFReposApiEndpoint, 'GET')
+  .then(obj => getHtmlRepoList(obj))
+  .then(html => renderList($repoList, html))
+  .catch(() => renderList($repoList)('<li>Error</li>'))
 
-Request(HYFMembersApiEndpiont, 'GET')
-  .then(GetHtmlMemberList)
-  .then(RenderList.bind(null, $memberList))
-  .catch(RenderList.bind(null, $memberList, '<li>Error</li>'))
+fetchJSON(HYFMembersApiEndpoint)
+  .then(getHtmlMemberList)
+  .then(renderList2($memberList))
+  .catch(() => renderList($memberList)('<li>Error</li>'))
